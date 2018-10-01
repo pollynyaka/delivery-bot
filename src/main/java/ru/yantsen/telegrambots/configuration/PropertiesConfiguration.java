954976f3d187317e433d534fccc11f8e7f315bff
Polly <pollynyaka@gmail.com>
2018-10-01 13:07:14 +0300

    - use environment variables for bot properties file path;

--- src/main/java/ru/yantsen/telegrambots/configuration/PropertiesConfiguration.java
+++ src/main/java/ru/yantsen/telegrambots/configuration/PropertiesConfiguration.java
@@ -1,7 +1,9 @@
 package ru.yantsen.telegrambots.configuration;
 
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
@@ -11,21 +13,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Component
-public class PropertiesConfiguration {
+public class PropertiesConfiguration implements EnvironmentAware {
+
+    private Environment environment;
 
     @Bean
     public PropertyPlaceholderConfigurer properties() {
         final PropertyPlaceholderConfigurer ppc = new PropertyPlaceholderConfigurer();
         ppc.setIgnoreResourceNotFound(true);
 
-        final List<Resource> resourceLst = new ArrayList<Resource>();
+       final List<Resource> resourceLst = new ArrayList<Resource>();
         resourceLst.add(new ClassPathResource("proxy.properties"));
-        resourceLst.add(new FileSystemResource("/home/bo/Develop/spring/BotProject/bot.properties"));
+        resourceLst.add(new FileSystemResource(environment.getProperty("BOT_CONFIG_HOME")));
 
         ppc.setLocations(resourceLst.toArray(new Resource[]{}));
 
         return ppc;
     }
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
+    }
 }
-
-
