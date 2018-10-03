32536f70bbbb510ea10de949f91173270d5404d4
Polly <pollynyaka@gmail.com>
2018-10-03 14:04:07 +0300

    - add controller

--- src/main/java/ru/yantsen/telegrambots/ApplicationStart.java
+++ src/main/java/ru/yantsen/telegrambots/ApplicationStart.java
@@ -2,20 +2,19 @@ package ru.yantsen.telegrambots;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ApplicationStart {
+public class ApplicationStart extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ApplicationStart.class);
+    }
+
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(ApplicationStart.class, args);
-
-        while (true) {
-            try {
-                Thread.sleep(80000L);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
     }
 
 }
---
+++ src/main/java/ru/yantsen/telegrambots/controller/MainDeliveryBotController.java
@@ -0,0 +1,27 @@
+package ru.yantsen.telegrambots.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainDeliveryBotController {
+
+    @Value("${bot.username}")
+    private String botUsername;
+
+
+    @GetMapping("/")
+    public String showBotName() {
+        return "Telegram delivery bot name = " + botUsername;
+    }
+
+    @RequestMapping("/{word}")
+    String hello(@PathVariable String word) {
+        return "\"" + word + "\" - that's funny";
+    }
+
+
+}
